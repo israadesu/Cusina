3f66464d80e7c1af107e05b38ac26b9977f6633c
israadesu <120487742+israadesu@users.noreply.github.com>
2024-07-13 05:14:41 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,7 +19,7 @@ const DesktopSidebar = () => {
   const isFavoritesRoute = location.pathname === "/favorites";
 
   return (
-    <div className="p-3 md:p-5 border-r border-gray-800 min-h-screen w-24 md:w-64 hidden lg:block ">
+    <div className="p-1 md:p-5 border-r border-gray-800 min-h-screen w-16 md:w-64 hidden sm:block ">
       <div className="flex flex-col gap-20 sticky top-10 left-0 ">
         <div className="w-full">
           <img src="/main.png" alt="logo" className="hidden md:block h-auto " />
@@ -49,7 +49,7 @@ const MobileSidebar = () => {
 
   return (
     <div className="flex justify-center gap-10 border-gray-800 fixed w-full bg-inherit bottom-0 left-1/2 transform -translate-x-1/2 
-    bg-slate-50 border border-slate-300/80 z-10 p-3 lg:hidden rounded-t-2xl pb-1">
+    bg-slate-50 border border-slate-300/80 z-10 p-3 sm:hidden rounded-t-2xl pb-1">
 
       <Link to="/" className="flex flex-col items-center">
         <Home size={24} className={`cursor-pointer flex items-center ${isHomeRoute ? "text-blue-500" : ""}`} />
